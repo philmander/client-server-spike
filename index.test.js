58c88e3a9becd8f4d225e74f8755f8f10b83c0a2
Phil Mander <philip.mander@gmail.com>
2022-05-31 20:22:27 +0000

    Changed to mocha,

--- index.test.js
+++ index.test.js
@@ -1,8 +1,10 @@
-const tap = require('tap')
-tap.test('it calls a method on the server', t => {
+const mocha = require('mocha')
+const assert = require('assert')
+const server = require('./client')
 
-  const result = server.add(10, 10);
-
-  t.assert(result, 10);
-  
-});
+describe('Direct server calls', function () {
+  it('calls a method on the server', async t => {
+    const result = server.add(10, 10)
+    t.ok(result, 20)
+  })
+})
